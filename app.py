cf26b52777ab7ddd8177c725bcf196f4b547a9dd
Nawaz18here <quadirnawaj144@gmail.com>
2022-07-05 01:30:31 +0530

    Updated content of readme.md file

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app= Flask(__name__)
 
 @app.route("/",methods=["GET","POST"])
 def index():
-    return "Starting my first machine learning project"
+    return "CI CD pipeline has been established"
 
 if __name__=="__main__":
     app.run(debug=True)
