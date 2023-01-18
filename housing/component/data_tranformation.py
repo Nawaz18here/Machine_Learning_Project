50ee6ed3a3d0949aa77cf3da8bc7383b100470cd
Nawaz18here <quadirnawaj144@gmail.com>
2023-01-19 01:55:53 +0530

    checking my project

--- housing/component/data_tranformation.py
+++ housing/component/data_tranformation.py
@@ -4,6 +4,80 @@ from housing.entity.config_entity import DataTransformationConfig
 from housing.entity.artifact_entity import DataIngestionArtifact,\
      DataValidationArtifact
 import sys,os
+import numpy as np 
+from sklearn.base import BaseEstimator, TransformerMixin
+from sklearn.preprocessing import StandardScaler,OneHotEncoder
+from sklearn.pipeline import Pipeline
+from sklearn.compose import ColumnTransformer
+from sklearn.impute import SimpleImputer
+import pandas as pd
+from housing.constant import *
+
+#   longitude: float
+#   latitude: float
+#   housing_median_age: float
+#   total_rooms: float
+#   total_bedrooms: float
+#   population: float
+#   households: float
+#   median_income: float
+#   median_house_value: float
+#   ocean_proximity: category
+#   income_cat: float
+
+
+class FeatureGenerator(BaseEstimator, TransformerMixin):
+
+    def __init__(self, add_bedrooms_per_room=True,
+                 total_rooms_ix=3,
+                 population_ix=5,
+                 households_ix=6,
+                 total_bedrooms_ix=4, columns=None):
+        """
+        FeatureGenerator Initialization
+        add_bedrooms_per_room: bool
+        total_rooms_ix: int index number of total rooms columns
+        population_ix: int index number of total population columns
+        households_ix: int index number of  households columns
+        total_bedrooms_ix: int index number of bedrooms columns
+        """
+        try:
+            self.columns = columns
+            if self.columns is not None:
+                total_rooms_ix = self.columns.index(COLUMN_TOTAL_ROOMS)
+                population_ix = self.columns.index(COLUMN_POPULATION)
+                households_ix = self.columns.index(COLUMN_HOUSEHOLDS)
+                total_bedrooms_ix = self.columns.index(COLUMN_TOTAL_BEDROOM)
+
+            self.add_bedrooms_per_room = add_bedrooms_per_room
+            self.total_rooms_ix = total_rooms_ix
+            self.population_ix = population_ix
+            self.households_ix = households_ix
+            self.total_bedrooms_ix = total_bedrooms_ix
+        except Exception as e:
+            raise HousingException(e, sys) from e
+
+    def fit(self, X, y=None):
+        return self
+
+    def transform(self, X, y=None):
+        try:
+            room_per_household = X[:, self.total_rooms_ix] / \
+                                 X[:, self.households_ix]
+            population_per_household = X[:, self.population_ix] / \
+                                       X[:, self.households_ix]
+            if self.add_bedrooms_per_room:
+                bedrooms_per_room = X[:, self.total_bedrooms_ix] / \
+                                    X[:, self.total_rooms_ix]
+                generated_feature = np.c_[
+                    X, room_per_household, population_per_household, bedrooms_per_room]
+            else:
+                generated_feature = np.c_[
+                    X, room_per_household, population_per_household]
+
+            return generated_feature
+        except Exception as e:
+            raise HousingException(e, sys) from e
 
 
 
@@ -21,3 +95,5 @@ class DataTransformation:
 
         except Exception as e:
             raise HousingException(e,sys) from e
+
+#Just checking
